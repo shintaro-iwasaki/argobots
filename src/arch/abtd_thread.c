7156041d632badde2f0d585d964a3b1f49d9f5ec
Shintaro Iwasaki <siwasaki@anl.gov>
2018-04-22 15:21:37 -0500

    atomic: Use atomic operations where they are necessary.
    Use atomic operations introduced in the previous commit for efficiency and
    correctness.
    
    Changes:
    - Use acquire-loads in busy-while loops (e.g., loops waiting ext_signal.)
      Corresponding writes are replaced with release-stores.
    - Use test_and_set and clear to implement a spinlock for efficiency. The size of
      lock value becomes 8 bits (1 byte) because GCC's test_and_set only supports
      one byte.
    - Remove ABTI_PTR_SPINLOCK / UNLOCK macros and use ABTI_spinlock as much as
      possible except those three cases:
      - ABT_mutex: ABTI_spinlock only supports zero/non-zero lock while ABT_mutex
        uses multiple value for the handover mechanism.
      - ABT_htable_queue: locks need to be initialized by memory clear, which is not
        guaranteed by ABTI_spinlock (although zero-clear can initialize the current
        spinlock.)
      - valgrind: its lock is global and has no explicit initialization function,
        so it is difficult to call ABTI_spinlock_free.

--- src/arch/abtd_thread.c
+++ src/arch/abtd_thread.c
@@ -67,7 +67,8 @@ static inline void ABTD_thread_terminate(ABTI_thread *p_thread)
     ABTD_thread_context *p_fctx = &p_thread->ctx;
 
     /* Now, the ULT has finished its job. Terminate the ULT. */
-    if (p_thread->request & ABTI_THREAD_REQ_JOIN_MANY) {
+    if (ABTD_atomic_load_uint32((uint32_t *)&p_thread->request)
+        & ABTI_THREAD_REQ_JOIN_MANY) {
         /* ABT_thread_join_many case */
         p_thread->state = ABT_THREAD_STATE_TERMINATED;
 
@@ -82,7 +83,8 @@ static inline void ABTD_thread_terminate(ABTI_thread *p_thread)
             ABTI_thread *p_next;
             for (i = p_jarg->counter; i < p_jarg->num_threads; i++) {
                 p_next = ABTI_thread_get_ptr(p_jarg->p_threads[i]);
-                if (p_next->state != ABT_THREAD_STATE_TERMINATED) {
+                if (ABTD_atomic_load_uint32((uint32_t *)&p_next->state)
+                    != ABT_THREAD_STATE_TERMINATED) {
                     ABTI_xstream *p_xstream = p_thread->p_last_xstream;
                     ABTI_POOL_REMOVE(p_next->p_pool, p_next->unit, p_xstream);
                     ABTI_thread_put_req_arg(p_next, p_req_arg);
@@ -106,13 +108,16 @@ static inline void ABTD_thread_terminate(ABTI_thread *p_thread)
         }
     }
 
+    /* Since we use atomic_fetch_or to strictly check if waiting threads exist,
+     * "acquire" load is not needed. */
     if (p_fctx->p_link) {
         /* If p_link is set, it means that other ULT has called the join. */
         ABTI_thread *p_joiner = (ABTI_thread *)p_fctx->p_link;
         if (p_thread->p_last_xstream == p_joiner->p_last_xstream) {
             /* Only when the current ULT is on the same ES as p_joiner's,
              * we can jump to the joiner ULT. */
-            p_thread->state = ABT_THREAD_STATE_TERMINATED;
+            ABTD_atomic_store_uint32((uint32_t *)&p_thread->state,
+                                     ABT_THREAD_STATE_TERMINATED);
             LOG_EVENT("[U%" PRIu64 ":E%d] terminated\n",
                       ABTI_thread_get_id(p_thread),
                       p_thread->p_last_xstream->rank);
@@ -125,9 +130,10 @@ static inline void ABTD_thread_terminate(ABTI_thread *p_thread)
              * p_joiner here so that p_joiner's scheduler can resume it. */
             ABTI_thread_set_ready(p_joiner);
 
-            /* We don't need to use the atomic operation here because the ULT
+            /* We don't need to use the atomic OR operation here because the ULT
              * will be terminated regardless of other requests. */
-            p_thread->request |= ABTI_THREAD_REQ_TERMINATE;
+            ABTD_atomic_store_uint32(&p_thread->request,
+                                     ABTI_THREAD_REQ_TERMINATE);
         }
     } else {
         uint32_t req = ABTD_atomic_fetch_or_uint32(&p_thread->request,
@@ -135,9 +141,7 @@ static inline void ABTD_thread_terminate(ABTI_thread *p_thread)
         if (req & ABTI_THREAD_REQ_JOIN) {
             /* This case means there has been a join request and the joiner has
              * blocked.  We have to wake up the joiner ULT. */
-            while ((volatile abt_ucontext_t *)p_fctx->p_link == NULL) {
-                ABTD_atomic_mem_barrier();
-            }
+            while (ABTD_atomic_load_ptr((void **)&p_fctx->p_link) == NULL);
             ABTI_thread_set_ready((ABTI_thread *)p_fctx->p_link);
         }
     }
@@ -179,6 +183,7 @@ void ABTD_thread_cancel(ABTI_thread *p_thread)
 #if defined(ABT_CONFIG_USE_FCONTEXT)
     ABTD_thread_context *p_fctx = &p_thread->ctx;
 
+    /* acquire load is not needed here. */
     if (p_fctx->p_link) {
         /* If p_link is set, it means that other ULT has called the join. */
         ABTI_thread *p_joiner = (ABTI_thread *)p_fctx->p_link;
@@ -189,9 +194,7 @@ void ABTD_thread_cancel(ABTI_thread *p_thread)
         if (req & ABTI_THREAD_REQ_JOIN) {
             /* This case means there has been a join request and the joiner has
              * blocked.  We have to wake up the joiner ULT. */
-            while ((volatile abt_ucontext_t *)p_fctx->p_link == NULL) {
-                ABTD_atomic_mem_barrier();
-            }
+            while (ABTD_atomic_load_ptr((void **)&p_fctx->p_link) == NULL);
             ABTI_thread *p_joiner = (ABTI_thread *)p_fctx->p_link;
             ABTI_thread_set_ready(p_joiner);
         }
